71faf1fd77b57784a0ff1fb047de5d07f80a4983
JB <jeanbaptiste.geulin@gmail.com>
2023-11-24 15:58:24 +0100

    création component Panier

---
+++ src/components/Panier/Panier.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Panier.css';
+
+const Panier = () => (
+  <div className="Panier">
+    Panier Component
+  </div>
+);
+
+Panier.propTypes = {};
+
+Panier.defaultProps = {};
+
+export default Panier;
