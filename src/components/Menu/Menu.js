b18b381edf9b51641988b076fca06a0e5a079254
JB <jeanbaptiste.geulin@gmail.com>
2023-11-24 15:29:13 +0100

    crétion component Menu

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Menu.css';
+
+const Menu = () => (
+  <div className="Menu">
+    Menu Component
+  </div>
+);
+
+Menu.propTypes = {};
+
+Menu.defaultProps = {};
+
+export default Menu;
